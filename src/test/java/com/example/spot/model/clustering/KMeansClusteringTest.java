ab6f4c3aad2ac328be90b3d7db96a2ab5f118979
howudong <tjdvy963@naver.com>
2023-11-24 17:23:27 +0900

    test : K-means에 대한 테스트코드 추가

---
+++ src/test/java/com/example/spot/model/clustering/KMeansClusteringTest.java
@@ -0,0 +1,70 @@
+package com.example.spot.model.clustering;
+
+import com.example.spot.model.Location;
+import com.example.spot.model.MapType;
+import com.example.spot.model.Pin;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class KMeansClusteringTest {
+    @ParameterizedTest
+    @MethodSource("getClusters")
+    @DisplayName("임계값 안에 들어간다면, 모두 하나의 군집이 되어야 한다.")
+    void 군집_통일_검사(List<Pin> pins) {
+        //given
+        Clustering clustering = new KMeansClustering(pins, 100000);
+        //when
+        List<Pin> result = clustering.run(new Location(1.0, 2.0));
+        //then
+        assertThat(result).hasSize(1);
+
+    }
+    @ParameterizedTest
+    @MethodSource("getDifferentType")
+    @DisplayName("MapType이 다르다면, 다른 군집이 되어야한다.")
+    void 군집_분리_검사(List<Pin> pins) {
+        //given
+        Clustering clustering = new KMeansClustering(pins, 100000);
+        //when
+        List<Pin> result = clustering.run(new Location(1.0, 2.0));
+        //then
+        assertThat(result).hasSize(2);
+
+    }
+
+    public static Stream<Arguments> getClusters() {
+        return Stream.of(
+                Arguments.of(
+                        List.of(new Pin(new Location(1.0, 2.0), MapType.TOUR),
+                                new Pin(new Location(1.5, 2.0), MapType.TOUR),
+                                new Pin(new Location(1.3, 2.0), MapType.TOUR))
+                ),
+                Arguments.of(
+                        List.of(new Pin(new Location(1.0, 2.0), MapType.ACCIDENT),
+                                new Pin(new Location(2.5, 3.0), MapType.ACCIDENT),
+                                new Pin(new Location(2.3, 2.0), MapType.ACCIDENT))
+                ));
+    }
+    public static Stream<Arguments> getDifferentType() {
+        return Stream.of(
+                Arguments.of(
+                        List.of(new Pin(new Location(1.0, 2.0), MapType.TOUR),
+                                new Pin(new Location(1.5, 2.0), MapType.ACCIDENT),
+                                new Pin(new Location(1.3, 2.0), MapType.TOUR))
+                ),
+                Arguments.of(
+                        List.of(new Pin(new Location(1.0, 2.0), MapType.ACCIDENT),
+                                new Pin(new Location(2.5, 3.0), MapType.TOUR),
+                                new Pin(new Location(2.3, 2.0), MapType.ACCIDENT))
+                ));
+    }
+}
