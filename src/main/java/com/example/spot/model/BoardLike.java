3849faffaea3cf79239c31db27eeade168475086
YuneeeM <dana512@naver.com>
2023-11-24 21:00:53 +0900

    feat : board 작성 기능 추가

---
+++ src/main/java/com/example/spot/model/BoardLike.java
@@ -0,0 +1,33 @@
+package com.example.spot.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Table(name = "boardlikes")
+@Entity
+public class BoardLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long likeId;
+
+    // 다대일 관계 설정: 한 개의 Like는 하나의 User와 관련됨
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id")
+    private User user;
+
+    // 다대일 관계 설정: 한 개의 Like는 하나의 Board와 관련됨
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "boardId")
+    private Board board;
+
+    private Long checkLike;
+
+
+}
